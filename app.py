de54ba4cacd0ef90c0b99d01ce06b8a26fdf940a
MLewis03 <marymargaretlewis03@gmail.com>
2024-05-03 15:05:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ df = pd.read_csv("Survey_Responses_Cleaned - Sheet1.csv")
 
 df = df[df['UNCC_Affiliation'] != 'Alumni']
 
-df = df['Sex', "Cycle_Length", "Fertile_Window_Timing", "Fertile_Days", "Cycle_Phases_Order", "Ovulation_Dominant_Hormone", "Blood_Loss"]
-
 answers  = {
     'Cycle_Length': "28 days",
     'Fertile_Window_Timing': "A few days before ovulation",
@@ -26,10 +24,58 @@ for column, correct_answer in answers.items():
 
 df['Total_Knowledge_Score'] = df.filter(like='_score').sum(axis=1)
 
-st.table(df)
+df = df[['Sex', "Cycle_Length", "Fertile_Window_Timing", "Fertile_Days", "Cycle_Phases_Order", "Ovulation_Dominant_Hormone", "Blood_Loss", "Total_Knowledge_Score"]]
 
-# Make side by side sex chart
-# Get rid of underscores in variable names
-# Reset sex select box whenever variable selected changes
-# Use st.form to make the quiz submittable
-# Maybe include optional filter for sex or year in school using group.by in select boxes
+with st.form("my_form"):
+   st.write("Test Your Knowledge of Menstrual Cycles!")
+   selected_sex = st.radio('Are you male or female? ', ['Male',
+                                                        'Female'])
+   selected_length = st.radio('Approximately how long is a normal menstrual cycle? ', ['20 Days',
+                                                                                       '28 Days', 
+                                                                                       '36 Days'])
+   selected_window = st.radio('When during the menstrual cycle is the fertile window? ', ['Everyday',
+                                                                                          'Right Before the Start of a Period', 
+                                                                                          'A Few Days Before Ovulation'])
+   selected_days = st.radio('Approximately how many days in a cycle is a woman usually fertile? ', ['Two Days',
+                                                                                                    'Six Days', 
+                                                                                                    'Ten Days', 
+                                                                                                    'Fourteen Days'])
+   selected_order = st.radio('What is the order of the processes/phases of the menstrual cycle? ', ['Fertile Phase, Pre-menstrual, Period',
+                                                                                                    'Ovulation, Fertile Phase, Pre-menstrual, Menstrual Phase', 
+                                                                                                    'Lonianting Phase, Obseum Phase, Pre-menstrual, Menstrual Phase', 
+                                                                                                    'Menstruation, Follicular Phase, Ovulation, Luteal Phase'])
+   selected_hormone = st.radio('Which hormone is dominant only during ovulation? ', ['Progesterone',
+                                                                                    'Luteinizing hormone', 
+                                                                                    'Estrogen',
+                                                                                    'Cortisol'])
+   selected_loss = st.radio('How much blood is typically lost during a period? ', ['Two tablespoons',
+                                                                                    'Quarter Cup', 
+                                                                                    'Half a Cup',
+                                                                                    'Whole Cup'])
+   submitted = st.form_submit_button("Submit and Grade")
+
+   if submitted:
+       st.balloons()
+       st.success('Your quiz has been submitted!')
+
+quiz_df = pd.DataFrame(columns=['Sex', 'Cycle_Length', 'Fertile_Window_Timing', 'Fertile_Days', 'Cycle_Phases_Order', 
+                                'Ovulation_Dominant_Hormone', 'Blood_Loss'])
+quiz_df = {
+    'Sex': [selected_sex],
+    'Cycle_Length': [selected_length],
+    'Fertile_Window_Timing': [selected_window],
+    'Fertile_Days': [selected_days],
+    'Cycle_Phases_Order': [selected_order],
+    'Ovulation_Dominant_Hormone': [selected_hormone],
+    'Blood_Loss': [selected_loss]
+}
+
+if submitted:
+       st.write("Done")
+
+# To-Do
+#   Figure out how to reset the radio buttons when page is first opened
+#   Grade the quiz and make total score
+#   Display correct answers with success and error messages
+#   Make it such that the df keeps growing and is saved
+#   Make by sex histograms to compare results with all others who have taken this quiz
